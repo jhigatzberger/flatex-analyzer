f29107214c29ec2f8b555fdf76f9b812c84c0860
jhigatzberger <j.higatzberger@gmail.com>
2025-06-10 16:20:25 +0200

    refactor: extract color-mode-toggle and repo-button into own components

---
+++ src/app/components/repo-button.tsx
@@ -0,0 +1,31 @@
+import { Button, ButtonProps, LinkProps, Stack, Typography } from "@mui/material";
+import { useGitHubStars } from "../hooks/use-github-stars";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import StarIcon from "@mui/icons-material/Star";
+import { AnchorHTMLAttributes } from "react";
+
+const REPO = "jhigatzberger/flatex-analyzer";
+
+
+
+export function RepoButton(props: AnchorHTMLAttributes<HTMLAnchorElement> & ButtonProps) {
+    const stars = useGitHubStars(REPO);
+    return <Button
+        variant="outlined"
+        size="large"
+        color="secondary"
+        startIcon={<GitHubIcon />}
+        href={`https://github.com/${REPO}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        {...props}
+    >
+        GitHub{" "}
+        {stars !== null && (
+            <Stack direction="row" spacing={0.5} alignItems="center" ml={1}>
+                <StarIcon fontSize="small" />
+                <Typography variant="body2">{stars}</Typography>
+            </Stack>
+        )}
+    </Button>
+}

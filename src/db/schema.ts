08ea2aed0aed1109758238f6aa48f747ce9814fe
jhigatzberger <j.higatzberger@gmail.com>
2025-06-11 15:33:39 +0200

    refactor: remove postgres database

--- src/db/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, date, jsonb } from "drizzle-orm/pg-core";
-
-export const exchangeRatesTable = pgTable("exchange_rates", {
-  date: date().primaryKey(), // e.g. '2025-05-31'
-  conversion_rates: jsonb().notNull(), // store the whole conversion_rates object
-});

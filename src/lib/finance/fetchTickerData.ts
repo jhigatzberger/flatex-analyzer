ace05f3158924eb08f46b770e4953da62eb19c1f
jhigatzberger <j.higatzberger@gmail.com>
2025-06-12 14:58:22 +0200

    fix: use getEnv() instead of env

--- src/lib/finance/fetchTickerData.ts
+++ src/lib/finance/fetchTickerData.ts
@@ -2,7 +2,7 @@ import { getEnv } from "../env";
 import { FullTickerData } from "./schemas";
 
 export async function fetchTickerData(ticker: string): Promise<FullTickerData> {
-  const url = `${env.YAHOO_FINANCE_WRAPPER_URL}stock/${ticker}`;
+  const url = `${getEnv().YAHOO_FINANCE_WRAPPER_URL}stock/${ticker}`;
 
   const response = await fetch(url);
   if (!response.ok) {

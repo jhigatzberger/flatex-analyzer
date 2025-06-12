4376ca8ace3dd1d8d67e7dfc016a0dcbe247333c
Seschoscho <j.higatzberger@gmail.com>
2025-06-12 21:04:46 +0200

    style: info color

--- src/app/components/info-box.tsx
+++ src/app/components/info-box.tsx
@@ -1,22 +1,26 @@
 // components/TransparentInfoBox.tsx
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { alpha, Box, Typography, useTheme } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 
 export default function InfoBox({ text }: { text: string }) {
+  
+  const theme = useTheme();
+  const mainColor = alpha(theme.palette.secondary.main, 0.1);
+  const borderColor = alpha(theme.palette.secondary.main, 0.5);
   return (
     <Box
       display="flex"
       alignItems="center"
-      bgcolor="rgba(255, 165, 0, 0.1)" // light transparent orange
-      border={`1px solid rgba(255, 165, 0, 0.5)`} // soft orange border
+      bgcolor={mainColor} // light transparent orange
+      border={`1px solid ${borderColor}`} // soft orange border
       borderRadius={2}
       px={2}
       py={1.5}
       my={2}
     >
-      <InfoIcon sx={{ color: "orange", mr: 1 }} />
-      <Typography variant="body2" color="orange">
+      <InfoIcon color="secondary" sx={{ mr: 1 }} />
+      <Typography variant="body2" color="secondary">
         {text}
       </Typography>
     </Box>
